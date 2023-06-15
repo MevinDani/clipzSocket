a94d4225bbfb9a830a751f1b5632130b089e286c
MEVINDANI <mevindani14@gmail.com>
2023-06-15 10:18:26 +0530

    socket clipz

---
+++ index.js
@@ -0,0 +1,65 @@
+const io = require("socket.io")(8900, {
+    cors: {
+        origin: "http://localhost:4200"
+    }
+})
+
+let users = [];
+
+const addUser = (locUserId, socketId) => {
+    !users.some((user) => user.locUserId == locUserId) &&
+        users.push({ locUserId, socketId })
+    console.log(users);
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (receiverId) => {
+    return users.find(user => user.locUserId == receiverId)
+}
+
+io.on("connection", (socket) => {
+    // connection
+    console.log("a user connected");
+    io.emit('welcome', 'welcome from socket server')
+
+    // take uderId and Socketid
+    socket.on("addUser", locUserId => {
+        addUser(locUserId, socket.id)
+        io.emit("getUsers", users)
+        // console.log(users, 'adduser');
+    })
+
+    // send and get messages
+    socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+        console.log(users, 'sndmsg');
+        console.log(senderId, receiverId, message);
+        const user = getUser(receiverId)
+        // console.log(user);
+        if (user) {
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                message
+            })
+        }
+    })
+
+    // delete msgs
+    socket.on("deleteMessage", ({ messageId, senderId, receiverId }) => {
+        const user = getUser(receiverId)
+        if (user) {
+            io.to(user.socketId).emit("deletedMessage", {
+                messageId
+            })
+        }
+    })
+
+    // handle disconnect
+    socket.on("disconnect", () => {
+        console.log("a user disconnects");
+        removeUser(socket.id)
+        io.emit("getUsers", users)
+    })
+});
